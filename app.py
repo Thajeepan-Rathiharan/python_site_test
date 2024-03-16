49808715c284628bf98ef50d1e8880ff118d883a
Thajeepan-Rathiharan <thajee2002@gmail.com>
2024-03-16 11:13:26 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "this is the homepage"
+app.register_blueprint(views, url_prefix="/")
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
